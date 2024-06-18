74567a60db9f6bcaea02792893e110436cd39436
AMRIT <tamangamrit767@gmail.com>
2024-06-18 23:00:20 +0545

    Adding set

--- practice/sets/main.dart
+++ practice/sets/main.dart
@@ -1,5 +1,10 @@
 void main() {
   Set<String> names = {'Amrit', 'Rohan', 'Sarthak'};
   names.add('Amrit');
+
+  // since i added the element which is already present in the set the compiler will ignore the 'Amrit' that is added
+
+  //  for sets there is no any insert function as it has no any order
+
   print(names);
 }
